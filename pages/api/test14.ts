ec5c4181c009f72808fb7470f3ba40c8f81f1e1e
Mot163 <modcloud@163.com>
2023-01-23 21:10:17 +0800

    test

--- pages/api/test14.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-async function test(req: NextApiRequest, res: NextApiResponse) {
-    res.json({ code: 200, message: 'test14' });
-}
-
-export default test;
--- pages/api/test7.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-async function test(req: NextApiRequest, res: NextApiResponse) {
-    res.json({ code: 200, message: 'test7' });
-}
-
-export default test;
