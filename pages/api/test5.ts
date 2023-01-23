3975e6d372191bbe7e6eb04884e74661ba34edf7
Mot163 <modcloud@163.com>
2023-01-23 20:58:26 +0800

    test

---
+++ pages/api/test5.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+async function test(req: NextApiRequest, res: NextApiResponse) {
+    res.json({ code: 200, message: 'test5' });
+}
+
+export default test;
